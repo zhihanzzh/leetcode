fcf42ca95cba975b35a1ad5c462c8555cd13c012
zhihanzzh <zzhzhangzhihan@yahoo.com>
2017-09-27 22:01:09 -0400

    solution

---
+++ HashTable/389FindtheDifference/solution1.java
@@ -0,0 +1,8 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char res=0;
+        for(char c: s.toCharArray())    res-=c;
+        for(char c: t.toCharArray())    res+=c;
+        return res;
+    }
+}
