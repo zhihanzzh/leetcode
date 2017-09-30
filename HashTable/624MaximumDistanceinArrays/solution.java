00fa4a6a09b34112867df75afad781a45288ffa6
zhihanzzh <zzhzhangzhihan@yahoo.com>
2017-09-29 23:32:24 -0400

    solution

---
+++ HashTable/624MaximumDistanceinArrays/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDistance(List<List<Integer>> arrays) {
+        int result = Integer.MIN_VALUE;
+        int min = arrays.get(0).get(0);
+        int max = arrays.get(0).get(arrays.get(0).size() - 1);
+        
+        for(int i = 1; i < arrays.size(); i++){
+            result = Math.max(result, Math.abs(arrays.get(i).get(arrays.get(i).size() - 1) - min));
+            result = Math.max(result, Math.abs(max - arrays.get(i).get(0)));
+            min = Math.min(min, arrays.get(i).get(0));
+            max = Math.max(max, arrays.get(i).get(arrays.get(i).size() - 1));
+        }
+        
+        return result;
+    }
+}
---
+++ HashTable/624MaximumDistanceinArrays/solution1.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int maxDistance(List<List<Integer>> arrays) {
+        int max = arrays.get(0).get(arrays.get(0).size() - 1);       
+        int min = arrays.get(0).get(0); 
+        int maxIndex = 0, minIndex = 0;
+        
+        //Find min & max values
+        for(int i = 1; i < arrays.size(); i++){
+            
+            List<Integer> list = arrays.get(i);
+       
+            if(list.get(0) < min){          
+                min = list.get(0);
+                minIndex = i;   
+            }
+            
+            if(list.get(list.size() - 1) > max){          
+                max = list.get(list.size() - 1);
+                maxIndex = i;   
+            }
+                        
+        }
+        
+        //If min & max indices are different, return
+        if(minIndex != maxIndex) return max - min;
+        
+        //Otherwise, min & max indices are the same. Need to decide whether to:
+        // - Keep min and find second highest max from another index
+        // - Keep max and find second lowest min from another index
+        
+        int distMin = Integer.MIN_VALUE, distMax = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < maxIndex; i++){
+            
+            List<Integer> list = arrays.get(i);        
+            distMin = Math.max(distMin, list.get(list.size() - 1) - min);
+            distMax = Math.max(distMax, max - list.get(0));
+            
+        }
+
+        for(int i = maxIndex + 1; i < arrays.size(); i++){
+            
+            List<Integer> list = arrays.get(i);        
+            distMin = Math.max(distMin, list.get(list.size() - 1) - min);
+            distMax = Math.max(distMax, max - list.get(0));
+            
+        }
+        
+        return Math.max(distMin, distMax);
+    }
+}
