d82346cf5436e061139ac93601bbd295c339d8ba
zhihanzzh <zzhzhangzhihan@yahoo.com>
2017-10-03 17:52:44 -0400

    solution

---
+++ HashTable/138CopyListwithRandomPointer/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if(head == null){
+            return null;
+        }
+        Map<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
+        
+        RandomListNode cur = head;
+        while(cur != null){
+            map.put(cur, new RandomListNode(cur.label));
+            cur = cur.next;
+        }
+        
+        for (Map.Entry<RandomListNode, RandomListNode> entry : map.entrySet()) {
+            RandomListNode newNode = entry.getValue();
+            newNode.next = map.get(entry.getKey().next);
+            newNode.random = map.get(entry.getKey().random);
+        }
+        
+        return map.get(head);
+    }
+}
