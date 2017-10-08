43c570928c5a66f2b17fcefebbad21bb8d69669d
zhihanzzh <zzhzhangzhihan@yahoo.com>
2017-10-08 16:43:20 -0400

    preorder solution

---
+++ HashTable/144BinaryTreePreorderTraversal/solution.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    List<Integer> result = new ArrayList<Integer>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root == null){
+            return result;
+        }
+        result.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+        return result;
+    }
+}
