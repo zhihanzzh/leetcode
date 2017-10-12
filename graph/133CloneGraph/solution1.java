1e54c60cfc9d1a8d1f94ff886e6e9ea52d754021
zhihanzzh <zzhzhangzhihan@yahoo.com>
2017-10-11 22:55:39 -0400

    solution

---
+++ graph/133CloneGraph/solution1.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     List<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<UndirectedGraphNode, UndirectedGraphNode>(); 
+        return clone(node, map);
+
+    }
+    
+    private UndirectedGraphNode clone(UndirectedGraphNode node, Map<UndirectedGraphNode, UndirectedGraphNode> map) {
+        if(node == null){
+            return null;
+        }
+        
+        if(map.containsKey(node)){
+            return map.get(node);
+        }
+        
+        UndirectedGraphNode copyNode = new UndirectedGraphNode(node.label);
+        map.put(node, copyNode);
+        for(UndirectedGraphNode neighbor : node.neighbors){
+            copyNode.neighbors.add(clone(neighbor, map));
+        }
+        
+        return copyNode;
+    }
+}
