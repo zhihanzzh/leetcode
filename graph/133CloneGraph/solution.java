14c18918e9b48e95d51f4137824562c61e5c284e
zhihanzzh <zzhzhangzhihan@yahoo.com>
2017-10-11 21:23:40 -0400

    solution

--- graph/133CloneGraph/solution.java
+++ graph/133CloneGraph/solution.java
@@ -11,37 +11,26 @@ public class Solution {
         if (node == null){
             return null;
         }
-        
         Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
         Set<UndirectedGraphNode> set = new HashSet<UndirectedGraphNode>();
         Queue<UndirectedGraphNode> que = new LinkedList<UndirectedGraphNode>();
         
         que.offer(node);
         set.add(node);
+        map.put(node, new UndirectedGraphNode(node.label));
         while(!que.isEmpty()){
             UndirectedGraphNode cur = que.poll();
             for(UndirectedGraphNode i : cur.neighbors){
                 if(!set.contains(i)){
+                    map.put(i, new UndirectedGraphNode(i.label));
                     que.offer(i);
                     set.add(i);
                 }
-            }
-            map.put(cur, new UndirectedGraphNode(cur.label));
-        }
-        set.clear();
-        
-        que.offer(node);
-        set.add(node);
-         while(!que.isEmpty()){
-            UndirectedGraphNode cur = que.poll();
-            for(UndirectedGraphNode i : cur.neighbors){
                 map.get(cur).neighbors.add(map.get(i));
-                if(!set.contains(i)){
-                    que.offer(i);
-                    set.add(i);
-                }
             }
+            
         }
+        
         
         return map.get(node);
     }
