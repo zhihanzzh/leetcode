f335727525d8c5953baacca6c46223f1e68fdad6
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-01-22 23:20:24 -0500

    solution

---
+++ String/344ReverseString/solution3.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public String reverseString(String s) {
+        int length = s.length();
+        if (length <= 1) return s;
+        String leftStr = s.substring(0, length / 2);
+        String rightStr = s.substring(length / 2, length);
+        return reverseString(rightStr) + reverseString(leftStr);
+    }
+}
