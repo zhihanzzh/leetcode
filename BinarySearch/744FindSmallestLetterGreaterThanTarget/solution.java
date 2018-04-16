e21d1b8a22902d94934f8a2b1947c58e1a223635
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-04-16 17:33:18 -0400

    solution

--- BinarySearch/744FindSmallestLetterGreaterThanTarget/solution.java
+++ BinarySearch/744FindSmallestLetterGreaterThanTarget/solution.java
@@ -1,13 +1,7 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        boolean[] seen = new boolean[26];
         for (char c: letters)
-            seen[c - 'a'] = true;
-
-        while (true) {
-            target++;
-            if (target > 'z') target = 'a';
-            if (seen[target - 'a']) return target;
-        }
+            if (c > target) return c;
+        return letters[0];
     }
 }
