902b9d75829dc3da69134aad7f71610ad95e16b7
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-05-12 17:54:03 -0400

    solution

---
+++ BitManipulation/693BinaryNumberwithAlternatingBits/solution1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int cur = n % 2;
+        n /= 2;
+        while (n > 0) {
+            if (cur == n % 2) {
+                return false;
+            }
+            
+            cur = n % 2;
+            n /= 2;
+        }
+        
+        return true;
+    }
+}
