9177040b456aa60f36289561b6664457bc03c8d1
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-05-13 23:56:03 -0400

    solution

---
+++ BitManipulation/338CountingBits/solution1.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        int i = 0, b = 1;
+        // [0, b) is calculated
+        while (b <= num) {
+            // generate [b, 2b) or [b, num) from [0, b)
+            while(i < b && i + b <= num){
+                ans[i + b] = ans[i] + 1;
+                ++i;
+            }
+            i = 0;   // reset i
+            b <<= 1; // b = 2b
+        }
+        return ans;
+    }
+}
