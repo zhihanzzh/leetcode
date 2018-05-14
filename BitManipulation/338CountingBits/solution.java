dcb4f8dae0fae47b38f2945f9f1ee54765f34952
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-05-13 23:55:17 -0400

    solution

---
+++ BitManipulation/338CountingBits/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        for (int i = 0; i <= num; ++i) {
+            ans[i] = popcount(i);
+        }
+        return ans;
+    }
+    private int popcount(int x) {
+        int count;
+        for (count = 0; x != 0; ++count)
+          x &= x - 1; //zeroing out the least significant nonzero bit
+        return count;
+    }
+}
