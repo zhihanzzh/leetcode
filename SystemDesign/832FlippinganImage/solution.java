f1b313a81f7972d1232adf6065a1ad207e259d8f
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-05-31 17:24:54 -0400

    solution

---
+++ SystemDesign/832FlippinganImage/solution.java
@@ -0,0 +1,32 @@
+public class Codec {
+
+    String chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    HashMap<String, String> map = new HashMap<>();
+    int count = 1;
+    // Encodes a URL to a shortened URL.
+    public String getString() {
+        int c = count;
+        StringBuilder sb = new StringBuilder();
+        while (c > 0) {
+            c--;
+            sb.append(chars.charAt(c % 62));
+            c /= 62;
+        }
+        return sb.toString();
+    }
+    public String encode(String longUrl) {
+        String key = getString();
+        map.put(key, longUrl);
+        count++;        
+        return "http://tinyurl.com/" + key;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return map.get(shortUrl.replace("http://tinyurl.com/", ""));
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
