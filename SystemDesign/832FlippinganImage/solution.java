158ffb93dae4d97680033036b77b6ea6778613f2
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-05-31 17:25:38 -0400

    solution

--- SystemDesign/832FlippinganImage/solution.java
+++ SystemDesign/832FlippinganImage/solution.java
@@ -1,3 +1,14 @@
+/*
+Using increasing numbers as codes like that is simple but has some disadvantages, which the below solution fixes:
+
+If I'm asked to encode the same long URL several times, it will get several entries. That wastes codes and memory.
+People can find out how many URLs have already been encoded. Not sure I want them to know.
+People might try to get special numbers by spamming me with repeated requests shortly before their desired number comes up.
+Only using digits means the codes can grow unnecessarily large. Only offers a million codes with length 6 (or smaller). Using six digits or lower or upper case letters would offer (10+26*2)6 = 56,800,235,584 codes with length 6.
+
+*/
+
+
 public class Codec {
 
     String chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
