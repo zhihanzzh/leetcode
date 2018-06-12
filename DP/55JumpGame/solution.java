b7aa2cbc6c6e001450adda100c97bbff714398ce
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-06-11 21:35:00 -0400

    solution

---
+++ DP/55JumpGame/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        boolean[] res = new boolean[nums.length];
+        
+        res[0] = true;
+        
+        for (int i = 1; i < res.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (res[j] == true && nums[j] + j >= i) {
+                    res[i] = true;
+                }
+            }
+        }
+        
+        return res[res.length - 1];
+    }
+}
