0f5a999bda363d87a37226026f91df4edf8f3828
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-06-11 21:22:50 -0400

    solution

---
+++ DP/70ClimbingStairs/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        
+        if (n == 1) {
+            return 1;
+        }
+        int[] res = new int[n + 1];
+        res[0] = 1;
+        res[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            res[i] = res[i - 1] + res[i - 2];
+        }
+        
+        return res[n];
+        
+    }
+}
