25ed944d0a1085ea54c200479fdd962b30908656
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-06-27 21:06:47 -0400

    solution

---
+++ tree/654MaximumBinaryTree/solution1.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        for(int i = 0; i < nums.length; i++) {
+            TreeNode curr = new TreeNode(nums[i]);
+            while(!stack.isEmpty() && stack.peek().val < nums[i]) {
+                curr.left = stack.pop();
+            }
+            if(!stack.isEmpty()) {
+                stack.peek().right = curr;
+            }
+            stack.push(curr);
+        }
+        
+        return stack.isEmpty() ? null : stack.removeLast();
+    }
+}
