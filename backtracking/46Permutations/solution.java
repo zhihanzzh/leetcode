cb1f8156247f8c074395a6c5ea949c5c64b69233
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-07-01 20:27:42 -0400

    solution

--- backtracking/46Permutations/solution.java
+++ backtracking/46Permutations/solution.java
@@ -1,18 +1,18 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        backtracking(res, new ArrayList<>(), nums, 0);
+        backtracking(res, new ArrayList<>(), nums);
         return res;
     }
     
-    private void backtracking(List<List<Integer>> res, List<Integer> temp, int[] nums, int start)  {
+    private void backtracking(List<List<Integer>> res, List<Integer> temp, int[] nums)  {
         if (temp.size() == nums.length) {
             res.add(new ArrayList<>(temp));
         } else {
             for (int i = 0; i < nums.length; i++) {
                 if (!temp.contains(nums[i])) {
                     temp.add(nums[i]);
-                    backtracking(res, temp, nums, i + 1);
+                    backtracking(res, temp, nums);
                     temp.remove(temp.size() - 1);
                 }
 
