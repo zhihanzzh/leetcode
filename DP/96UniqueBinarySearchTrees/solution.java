2ca1a0034fe427f8c2d91fa729f5a5988ab9ab90
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-07-16 19:09:08 -0400

    solution

---
+++ DP/96UniqueBinarySearchTrees/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numTrees(int n) {
+        if (n < 1) return 0;
+        int[] res = new int[n + 1];
+        
+        res[0] = 1;
+        res[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                res[i] += res[j] * res[i - j - 1];
+            }
+        }
+        
+        return res[n];
+    }
+}
