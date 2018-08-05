12ffe2517a521fe73d599e9c8d47a9d9bebc7dc0
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-08-04 23:16:02 -0400

    solution

--- LinkedList/21MergeTwoSortedLists/solution.java
+++ LinkedList/21MergeTwoSortedLists/solution.java
@@ -8,42 +8,29 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
-        }
-        if (l2 == null) {
-            return l1;
-        }
         
-        ListNode res = null;
-        if (l1.val < l2.val) {
-            res = l1;
-            l1 = l1.next;
-        } else {
-            res = l2;
-            l2 = l2.next;
-        }
-        ListNode t1 = l1;
-        ListNode t2 = l2;
-        ListNode cur = res;
+        ListNode res = new ListNode(0);
+        ListNode head = res;
         while (l1 != null && l2 != null) {
             if (l1.val < l2.val) {
-                cur.next = l1;
-                cur = cur.next;
+                head.next = l1;
+                head = head.next;
                 l1 = l1.next;
             } else {
-                cur.next = l2;
-                cur = cur.next;
+                head.next = l2;
+                head = head.next;
                 l2 = l2.next;
             }
         }
         
-        if (l1 == null) {
-            cur.next = l2;
-        } else {
-            cur.next = l1;
+        if (l1 != null) {
+            head.next = l1;
         }
         
-        return res;
+        if (l2 != null) {
+            head.next = l2;
+        }
+        
+        return res.next;
     }
 }
