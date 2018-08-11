a375d117582b87407985177868269710ae9d76c1
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-08-10 20:30:04 -0400

    solution

---
+++ Array/334IncreasingTripletSubsequence/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= min) {
+                min = nums[i];
+            } else if (nums[i] <= max) {
+                max = nums[i];
+            } else {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
